65cb534abd4ef9ceb4a2e03d13d66de42d0aeb8d
Gavin Fisher <gdf73278@iastate.edu>
2023-12-13 11:33:43 -0600

    format

--- WikiCrawler.java
+++ WikiCrawler.java
@@ -20,7 +20,6 @@ public class WikiCrawler {
     String fileName;
 
     public WikiCrawler(String seedUrl, String[] keywords, int max, String fileName) {
-
         this.seedUrl = seedUrl;
         this.keywords = keywords;
         this.max = max;
@@ -31,23 +30,21 @@ public class WikiCrawler {
     public void crawl() throws IOException {
         Queue<String> queue = new LinkedList<>();
         Set<String> visited = new HashSet<>();
-
         queue.add(seedUrl);
 
         while (!queue.isEmpty() && visited.size() < max) {
             String url = queue.poll();
             visited.add(url);
-
             Document doc = (Document) Jsoup.connect(url).get();
             Elements links = doc.select("p a[href]");
 
             for (Element link : links) {
                 String absUrl = link.attr("abs:href");
-
                 if (!visited.contains(absUrl) && absUrl.startsWith("https://en.wikipedia.org")) {
                     queue.add(absUrl);
                 }
             }
+
         }
 
         writeGraphToFile(visited);
@@ -55,7 +52,6 @@ public class WikiCrawler {
 
     private void writeGraphToFile(Set<String> visited) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter("graph.txt"));
-
         writer.write(visited.size() + "\n");
 
         for (String url : visited) {
@@ -65,16 +61,12 @@ public class WikiCrawler {
         writer.close();
     }
 
-    public static void main(String[] args) throws IOException {
-        WikiCrawler crawler = new WikiCrawler("https://en.wikipedia.org/wiki/Main_Page", 100);[^1^][1]
-        crawler.crawl();
-    }
 
     public boolean isInRobots(String endpoint) throws IOException {
         URL url = new URL("https://en.wikipedia.org/robots.txt");
         BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-
         String line;
+
         while ((line = reader.readLine()) != null) {
             if (line.startsWith("Disallow:")) {
                 String disallowedPage = line.substring("Disallow:".length()).trim();
@@ -84,6 +76,7 @@ public class WikiCrawler {
                 }
             }
         }
+
         reader.close();
         return false;
     }
